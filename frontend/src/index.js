59941bdbc5964f3673ae8219bb78639f2afa2abc
tyler <tylerruby0314@yahoo.com>
2021-02-18 15:46:46 -0600

    Front end setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider as ReduxProvider } from 'react-redux';
 import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import configureStore from './store';
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
   window.store = store;
 }
 
+
 function Root() {
   return (
-    <ReduxProvider store={store}>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </ReduxProvider>
+    </Provider>
   );
 }
+
 
 ReactDOM.render(
   <React.StrictMode>
