3e733f3eafbd5e39fb6d4400f859e4f74b82c96b
tyler <tylerruby0314@yahoo.com>
2021-02-18 12:09:33 -0600

    Added Routes for checking login/logout.

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,6 +5,11 @@ const { User } = require('../../db/models');
 const { restoreUser } = require('../../utils/auth.js');
 const { requireAuth } = require('../../utils/auth.js');
 
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,11 @@
-const express = require('express');
-const router = express.Router();
-const apiRouter = require('./api');
+const router = require('express').Router();
+const apiRouter = require('./api')
 
-router.use('/api', apiRouter);
+router.use('/api', apiRouter)
 
 router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.send('Hello World!');
+  });
 
 module.exports = router;
