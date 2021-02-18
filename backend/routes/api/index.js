19413dda984b23c512f4a9565b5ce56ab63894e9
tyler <tylerruby0314@yahoo.com>
2021-02-18 11:34:22 -0600

    Finished phase 3

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { restoreUser } = require('../../utils/auth.js');
+const { requireAuth } = require('../../utils/auth.js');
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
+
 
 module.exports = router;
