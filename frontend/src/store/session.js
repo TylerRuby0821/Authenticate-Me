d2c1e7ae5f0201691eff8cbb1559b3401fd0cd53
tyler <tylerruby0314@yahoo.com>
2021-02-19 14:57:09 -0600

    Logout thunk

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -52,6 +52,14 @@ const setUser = (user) => {
       return response;
   };
 
+  export const logout = () => async dispatch => {
+    const response = await csrfFetch('/api/session', {
+      method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+  }
+
   const initialState = { user: null };
 
   const sessionReducer = (state = initialState, action) => {
