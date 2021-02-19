dbbe1b0add27aa2d6fed50e8a7f19c2063a62693
tyler <tylerruby0314@yahoo.com>
2021-02-19 10:30:34 -0600

    configured login thunk for session

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
@@ -13,11 +14,9 @@ if (process.env.NODE_ENV !== 'production') {
   restoreCSRF();
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
-if (process.env.NODE_ENV !== 'production') {
-  window.store = store;
-}
 
 
 function Root() {
