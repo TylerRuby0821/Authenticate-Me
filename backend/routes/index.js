cc94c32916b817776648935219fc16015dc5e06a
tyler <tylerruby0314@yahoo.com>
2021-02-19 16:44:40 -0600

    Added PackageJson for heroku

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -32,9 +32,4 @@ if (process.env.NODE_ENV !== 'production') {
   });
 }
 
-router.get('/hello/world', function(req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
-  });
-
 module.exports = router;
